cf0750ac0d77e66053d0c17f8faa3e24ffdf532d
AhmedAlyoussef88 <138717756+AhmedAlyoussef88@users.noreply.github.com>
2024-08-13 22:49:37 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,13 +5,37 @@ Contains the number_of_subscribers function
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If an invalid subreddit is provided, the function returns 0.
+    
+    :param subreddit: The name of the subreddit to query.
+    :return: The number of subscribers or 0 if the subreddit is invalid.
+    """
+    # Define the URL to query
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    
+    # Set the User-Agent to avoid Too Many Requests error
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Reddit API Client/0.1)'}
+    
+    try:
+        # Make the GET request to the Reddit API
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        # Check if the response status code is 200 (OK)
+        if response.status_code == 200:
+            # Parse the JSON response to get the number of subscribers
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            # If the status code is not 200, return 0
+            return 0
+    except Exception:
+        # In case of any exceptions, return 0
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+# Example usage:
+if __name__ == '__main__':
+    print(number_of_subscribers("programming"))  # Should return the number of subscribers
+    print(number_of_subscribers("this_is_a_fake_subreddit"))  # Should return 0
